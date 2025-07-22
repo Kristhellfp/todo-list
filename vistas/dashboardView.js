810940d53c8251e3b58d836481aa1e133135aa6a
kristel <klfpaz@scl.edu.gt>
2025-07-22 08:13:05 -0600

    Rama header

---
+++ vistas/dashboardView.js
@@ -0,0 +1,17 @@
+export function dashboard() {
+
+ let seccion = document.createElement('seccion');
+    let h1 = document.createElement ('h1');
+    h1.innerText = "Hola mundo"
+    seccion.appendChild(h1);
+
+return seccion; 
+
+}
+
+document.body.appendChild(dashboard());
+
+
+
+
+
