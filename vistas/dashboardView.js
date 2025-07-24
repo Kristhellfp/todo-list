464a41e68afb09c162483014e63c9f4b7fc7b8db
kristel <klfpaz@scl.edu.gt>
2025-07-23 18:50:08 -0600

    ClaseHeader

--- vistas/dashboardView.js
+++ vistas/dashboardView.js
@@ -1,9 +1,11 @@
+import { header } from "../componentes/header/headerComponent.js";
+
 export function dashboard() {
 
  let seccion = document.createElement('seccion');
-    let h1 = document.createElement ('h1');
-    h1.innerText = "Hola mundo"
-    seccion.appendChild(h1);
+  
+//header
+seccion.appendChild(header());
 
 return seccion; 
 
